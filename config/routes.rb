10f0aafbf4be9eb403709753a82128c9ceba4a50
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-12-18 07:13:34 +0000

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  # コントローラーの記述を変更すると処理を行うように記述
+
+  devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+  #不要なルートはskipオプションでルートを削除
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
